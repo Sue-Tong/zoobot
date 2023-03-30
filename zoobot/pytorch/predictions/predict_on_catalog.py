ef568fe31105a62ea5930150e553ab1c65edfab6
Tong Su <90353234+Sue-Tong@users.noreply.github.com>
2023-03-29 22:43:14 -0400

    Update predict_on_catalog.py

--- zoobot/pytorch/predictions/predict_on_catalog.py
+++ zoobot/pytorch/predictions/predict_on_catalog.py
@@ -90,3 +90,4 @@ def predict(catalog: pd.DataFrame, model: pl.LightningModule, n_samples: int, la
     end = datetime.datetime.fromtimestamp(time.time())
     logging.info('Completed at: {}'.format(end.strftime('%Y-%m-%d %H:%M:%S')))
     logging.info('Time elapsed: {}'.format(end - start))
+    return 12
